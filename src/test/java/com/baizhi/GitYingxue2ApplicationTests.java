10dfdc8064612833a9ca6637365f3e58d160ae79
zhangyu <1151135085@qq.com>
2020-08-11 22:51:55 +0800

    git_yingxue2  的视图git提交1

---
+++ src/test/java/com/baizhi/GitYingxue2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitYingxue2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
